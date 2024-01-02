df9f4e7be788997e381b2b1ea7dca795647caafe
zhengmark <2199303790@qq.com>
2024-01-02 10:04:32 +0800

    增加教程中的部分内容，每个人要是在自己电脑运行的话需要自行更改config.xml中的password

---
+++ src/main/java/com/taotao/MyBatisDemo.java
@@ -0,0 +1,34 @@
+package com.taotao;
+
+import com.taotao.pojo.Tb_user;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class MyBatisDemo {
+    public static void main(String[] args) throws IOException {
+        //1.加载mybatis的核心配置文件，获取 SqlSessionFactory
+        String resource = "mybatis-config.xml"; //相对路径
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3.执行sql
+        List<Tb_user> users = sqlSession.selectList("test.selectAll");
+
+        System.out.println(users);
+
+        //4.释放资源
+        sqlSession.close();
+    }
+}
+
+
