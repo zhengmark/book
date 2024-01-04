a3b519b99dc0955ce1c0b6558cdedba1765b3305
chaofengming <3517229524@qq.com>
2024-01-04 14:04:40 +0800

    增加了购物车的添加和删除功能

--- src/main/java/com/taotao/MyBatisDemo.java
+++ src/main/java/com/taotao/MyBatisDemo.java
@@ -26,67 +26,71 @@ public class MyBatisDemo {
 //              String id = "123456";
 //              String name = "客户";
 //              String pwd = "20021114";
-
-
+                String birth = "2002";
+                String password = "0000";
+                String address = "上海理工大学";
                 String name_log = "zheng";
                 String pwd_log = "22222";
                 String book_name = "大数据可视化";
                 String book_id = "123";
+                String phone_num = "13256789";
                 String user_id = "123456";
                 String appraise = "这本书真好看";
                 Integer quantity_purchased = 5;
                 Integer order = 0;
 
 //这是测试注册函数
-                Register re = new Register();
-                String name = "zheng";
-                String pwd = "22222";
-                boolean a1 = re.register(name,pwd);
-                System.out.println(a1);
+//                Register re = new Register();
+//                String name = "zheng";
+//                String pwd = "22222";
+//                boolean a1 = re.register(name,pwd);
+//                System.out.println(a1);
 
 //这是测试登录函数
-                Map<String, Object> params_log = new HashMap<>();
-
-                params_log.put("user_name", name_log);
-                params_log.put("password", pwd_log);
-                LogIn log = new LogIn();
-                String a = log.logIn(name_log, pwd_log);
+//                Map<String, Object> params_log = new HashMap<>();
+//
+//                params_log.put("user_name", name_log);
+//                params_log.put("password", pwd_log);
+//                LogIn log = new LogIn();
+//                String a = log.logIn(name_log, pwd_log);
 
 //返回图书信息
-                    Map<String, Object> params_search_book = new HashMap<>();
-
-                    params_search_book.put("book_name",book_name);
-                    params_search_book.put("book_id",book_id);
-                    Book_search book_search = new Book_search();
-                    System.out.println(book_search.book_search(book_id));
+//                    Map<String, Object> params_search_book = new HashMap<>();
+//
+//                    params_search_book.put("book_name",book_name);
+//                    params_search_book.put("book_id",book_id);
+//                    Book_search book_search = new Book_search();
+//                    System.out.println(book_search.book_search(book_id));
 
 //插入用户评价
-                    Map<String, Object> params_insert_appraise = new HashMap<>();
-
-                    params_insert_appraise.put("appraise",appraise);
-                    params_insert_appraise.put("book_id",book_id);
-                    params_insert_appraise.put("user_id",user_id);
-                    UserAppraiser user_appraiser = new UserAppraiser();
-                    user_appraiser.addUserBookReview(user_id,book_id,appraise);
-                    System.out.println(appraise_all);
-
-//插入购物车信息
-                    ShoppingCartManager shoppingCartManager_search = new ShoppingCartManager();
-                    shoppingCartManager_search.addItemsToCart(user_id,book_id,quantity_purchased);
-
-//查询购物车信息
-                    ShopViewer shopViewer = new ShopViewer();
-                    shopViewer.show_shop(user_id,order);
-
-
-// 提交事务（如果需要的话）
-                    sqlSession.commit();
+//                    Map<String, Object> params_insert_appraise = new HashMap<>();
 //
-                    System.out.println(users);
-                    System.out.println(log);
+//                    params_insert_appraise.put("appraise",appraise);
+//                    params_insert_appraise.put("book_id",book_id);
+//                    params_insert_appraise.put("user_id",user_id);
+//                    UserAppraiser user_appraise = new UserAppraiser();
+//                    user_appraise.addUserBookReview(user_id,book_id,appraise);
+//                    System.out.println(appraise_all);
 
-                    System.out.println(appraise);
-                    System.out.println(shop);
+////插入购物车信息
+//                    Shop shop_search = new Shop();
+//                    shop_search.shop(user_id,book_id,quantity_purchased);
+//
+////查询购物车信息
+//                    Show_shop show_shop = new Show_shop();
+//                    show_shop.show_shop(user_id,order);
+
+
+////更改信息
+//                    Update_user_info up = new Update_user_info();
+//                    up.update(user_id,password,phone_num,address,birth);
+//                    sqlSession.commit();
+
+////购物车增加和删除
+                ShoppingCartManager add = new ShoppingCartManager();
+                add.addItemsToCart(user_id,book_id,5);
+                add.deleteItemsCart("123456","123");
+
 
 //4.释放资源
                     sqlSession.close();
--- src/main/java/com/taotao/ShoppingCartManager.java
+++ src/main/java/com/taotao/ShoppingCartManager.java
@@ -1,6 +1,7 @@
 package com.taotao;
 
 import com.taotao.pojo.Tb_book_info;
+import com.taotao.pojo.Tb_user;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -8,6 +9,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class ShoppingCartManager {
@@ -25,12 +29,44 @@ public class ShoppingCartManager {
 
             //2.获取SqlSession对象，用它来执行sql
             SqlSession sqlSession = sqlSessionFactory.openSession();
-            Random random = new Random();
+
+// 执行插入操作
+            Book_search book_search = new Book_search();
+//            (user_id,book_id,unit_price,inventory,quantity_purchased,shipments)
+
+            Tb_book_info book_info = book_search.book_search(book_id);
+//            System.out.println(book_info);
+            Map<String, Object> params = new HashMap<>();
+            params.put("user_id", user_id);
+            params.put("book_id", book_id);
+            params.put("unit_price", book_info.getUnit_price());
+            params.put("inventory", book_info.getInventory());
+            params.put("quantity_purchased", quantity_purchased);
+            sqlSession.insert("book.order_on_shop", params);
+            sqlSession.commit();
+            sqlSession.close();
+        }
+
+        /*
+        购物车删除功能，
+        传入参数user_id，book_id
+        将数据库中的对应行删除
+         */
+        public void deleteItemsCart(String user_id, String book_id)throws IOException {
+            String resource = "mybatis-config.xml"; //相对路径
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            //2.获取SqlSession对象，用它来执行sql
+            SqlSession sqlSession = sqlSessionFactory.openSession();
 
 // 执行插入操作
             Book_search book_search = new Book_search();
             Tb_book_info book_info = book_search.book_search(book_id);
-//            System.out.println(book_info);
+            Map<String, Object> params = new HashMap<>();
+            params.put("user_id",user_id);
+            params.put("book_id",book_id);
+            sqlSession.delete("book.delete_shop_cart", params);
 
             sqlSession.commit();
             sqlSession.close();
