9fc076fd33ee269588a076ff5fa4de0fdbc33c3d
Baguette <baguettebattlenet@gmail.com>
2024-01-04 12:41:11 +0800

    delete 1.html update pom.xml mysql version create MyBatisUtil.java update Book_search.java & User_appraise.java

--- src/main/java/com/taotao/Book_search.java
+++ src/main/java/com/taotao/Book_search.java
@@ -1,18 +1,12 @@
 package com.taotao;
 
 import com.taotao.pojo.Tb_book_info;
-import com.taotao.pojo.Tb_user;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public class Book_search {
     /*
@@ -23,35 +17,22 @@ public class Book_search {
     如果没有匹配成功，返回一本book_id为“1111”的图书信息
     返回类型为Tb_book_info
      */
-    public Tb_book_info book_search(String input)throws IOException {
 
-        //1.加载mybatis的核心配置文件，获取 SqlSessionFactory
-        String resource = "mybatis-config.xml"; //相对路径
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    public Tb_book_info book_search(String input) throws IOException {
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("book_name", input);
 
-        //2.获取SqlSession对象，用它来执行sql
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        Random random = new Random();
-
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("book_name", input);
-
-
-// 执行插入操作
-        List<Tb_book_info> book_search = sqlSession.selectList("search_book.search_book", params);
-        if (book_search.isEmpty()) {
-            Tb_book_info impossible = new Tb_book_info();
-            impossible.setId("1111");
-            System.out.println("该书不存在");
-            sqlSession.commit();
-            sqlSession.close();
-            return impossible;
-        } else {
-            sqlSession.commit();
-            sqlSession.close();
-            return book_search.get(0);
+            // 执行查询操作
+            List<Tb_book_info> bookSearch = sqlSession.selectList("search_book.search_book", params);
+            if (bookSearch.isEmpty()) {
+                Tb_book_info impossible = new Tb_book_info();
+                impossible.setId("1111");
+                System.out.println("该书不存在");
+                return impossible;
+            } else {
+                return bookSearch.get(0);
+            }
         }
     }
 }
--- src/main/java/com/taotao/User_appraise.java
+++ src/main/java/com/taotao/User_appraise.java
@@ -1,46 +1,27 @@
 package com.taotao;
 
-import com.taotao.pojo.Tb_appraise;
-import com.taotao.pojo.Tb_book_info;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public class User_appraise {
     /*
     用户评价功能
-    通过调用内部user_appraise，传入参数user_id,book_id,appraise
+    通过调用内部user_appraise，传入参数user_id, book_id, appraise
     在评价表中插入以上信息
      */
-    public void user_appraise(String user_id,String book_id,String appraise)throws IOException {
+    public void user_appraise(String user_id, String book_id, String appraise) throws IOException {
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("user_id", user_id);
+            params.put("book_id", book_id);
+            params.put("appraise", appraise);
 
-        //1.加载mybatis的核心配置文件，获取 SqlSessionFactory
-        String resource = "mybatis-config.xml"; //相对路径
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
-        //2.获取SqlSession对象，用它来执行sql
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        Random random = new Random();
-
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("user_id", user_id);
-        params.put("book_id",book_id);
-        params.put("appraise",appraise);
-
-
-// 执行插入操作
-        List<Tb_appraise> user_appraise = sqlSession.selectList("book.insert_appraise", params);
-        sqlSession.commit();
-        sqlSession.close();
+            // 执行插入操作
+            sqlSession.insert("book.insert_appraise", params);
+            sqlSession.commit();
+        }
     }
 }
